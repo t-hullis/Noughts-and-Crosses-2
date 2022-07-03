0c5900319aa5c0b6e655a3974b90dc847d028585
t-hullis <t.hullis@yahoo.com>
2022-07-03 11:22:34 +0000

    add basic javascript to get the board div and to create 9 square divs

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+let board = document.getElementById("theBoard");
+// array for board postions
+const postionArray = [];
+
+// use loop to create 3x3 board with correct classes/set up object on board
+for (let index = 0; index <= 8; index++) {
+    let div = document.createElement("div");
+    div.classList.add("square")
+}
