89378465143c14f1d0cb7b9f76dfebbee1b1a77b
t-hullis <t.hullis@yahoo.com>
2022-07-07 13:05:25 +0000

    add winning combos in array

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,6 +38,19 @@ function gameEnd() {
 }
 gameEnd();
 
+function winner() {
+    let conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    ];
+}
+
 
 
 console.log(postionArray)
